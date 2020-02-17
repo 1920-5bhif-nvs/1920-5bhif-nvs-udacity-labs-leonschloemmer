20687ee6eecb6a4d2dbc8bc854a5515058f0d982
Leon Schlömmer <leon.schloemmer@posteo.at>
2020-02-17 20:43:18 +0100

    4_16

--- dessert-pusher/app/src/main/java/com/example/android/dessertpusher/DessertTimer.kt
+++ dessert-pusher/app/src/main/java/com/example/android/dessertpusher/DessertTimer.kt
@@ -57,13 +57,13 @@ class DessertTimer(lifecycle: Lifecycle) : LifecycleObserver {
     @OnLifecycleEvent(Lifecycle.Event.ON_START)
     fun startTimer() {
         // Create the runnable action, which prints out a log and increments the seconds counter
-        runnable = Runnable {
+        runnable = Runnable { 
             secondsCount++
             Timber.i("Timer is at : $secondsCount")
             // postDelayed re-adds the action to the queue of actions the Handler is cycling
             // through. The delayMillis param tells the handler to run the runnable in
             // 1 second (1000ms)
-            handler.postDelayed(runnable, 1000) 
+            handler.postDelayed(runnable, 1000)
         }
 
         // This is what initially starts the timer
