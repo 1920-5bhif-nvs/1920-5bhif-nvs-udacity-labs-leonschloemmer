f91f93b2eb942a37278d9ea001bedabe78af2782
Leon Schlömmer <leon.schloemmer@posteo.at>
2020-02-18 19:01:54 +0100

    5_10

--- guess-it/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ guess-it/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -5,8 +5,73 @@ import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
 
+    // The current word
+    var word = ""
+
+    // The current score
+    var score = 0
+
+    // The list of words - the front of the list is the next word to guess
+    private lateinit var wordList: MutableList<String>
+
     init {
         Log.i("GameViewModel", "GameViewModel created")
+
+        resetList()
+        nextWord()
+    }
+
+    /**
+     * Resets the list of words and randomizes the order
+     */
+    fun resetList() {
+        wordList = mutableListOf(
+                "queen",
+                "hospital",
+                "basketball",
+                "cat",
+                "change",
+                "snail",
+                "soup",
+                "calendar",
+                "sad",
+                "desk",
+                "guitar",
+                "home",
+                "railway",
+                "zebra",
+                "jelly",
+                "car",
+                "crow",
+                "trade",
+                "bag",
+                "roll",
+                "bubble"
+        )
+        wordList.shuffle()
+    }
+
+
+    /**
+     * Moves to the next word in the list
+     */
+    fun nextWord() {
+        //Select and remove a word from the list
+        if (wordList.isEmpty()) {
+//            gameFinished()
+        } else {
+            word = wordList.removeAt(0)
+        }
+    }
+
+    fun onSkip() {
+        score--
+        nextWord()
+    }
+
+    fun onCorrect() {
+        score++
+        nextWord()
     }
 
     override fun onCleared() {
