18d7403d2460fcd6fdc031a4fa9eda7551d0a094
Leon Schlömmer <leon.schloemmer@posteo.at>
2020-02-18 18:51:42 +0100

    5_08

--- guess-it/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ guess-it/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -17,11 +17,13 @@
 package com.example.android.guesstheword.screens.game
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.NavHostFragment.findNavController
 import com.example.android.guesstheword.R
 import com.example.android.guesstheword.databinding.GameFragmentBinding
@@ -42,6 +44,8 @@ class GameFragment : Fragment() {
 
     private lateinit var binding: GameFragmentBinding
 
+    private lateinit var viewModel: GameViewModel
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
@@ -60,6 +64,10 @@ class GameFragment : Fragment() {
         binding.skipButton.setOnClickListener { onSkip() }
         updateScoreText()
         updateWordText()
+
+        viewModel = ViewModelProviders.of(this).get(GameViewModel::class.java)
+        Log.i("I/GameFragment", "Called Viewmodeproviders of")
+
         return binding.root
 
     }
