004dbdb356de9d42277d9b49f4e1780020e4074f
Leon Schlömmer <leon.schloemmer@posteo.at>
2020-02-20 16:11:00 +0100

    8_12

--- andfun-kotlin-mars-real-estate-Starter-Code/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ andfun-kotlin-mars-real-estate-Starter-Code/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -43,10 +43,11 @@ class OverviewViewModel : ViewModel() {
     private var viewModelJob = Job()
     private val coroutineScope = CoroutineScope(viewModelJob + Dispatchers.Main )
 
-    private val _property = MutableLiveData<MarsProperty>()
+    private val _properties = MutableLiveData<List<MarsProperty>>()
 
-    val property: LiveData<MarsProperty>
-        get() = _property
+    val properties: LiveData<List<MarsProperty>>
+        get() = _properties
+
 
     /**
      * Call getMarsRealEstateProperties() on init so we can display status immediately.
@@ -66,7 +67,7 @@ class OverviewViewModel : ViewModel() {
                 _status.value = "Success: ${listResult.size} Mars properties retrieved"
 
                 if (listResult.size > 0) {
-                    _property.value = listResult[0]
+                    _properties.value = listResult
                 }
             } catch (e: Exception) {
                 _status.value = "Failure: ${e.message}"
